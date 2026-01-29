ee0e18028fa374553ca16bcfb87c6375021924de
Cryptizism <cryptizism@protonmail.com>
2026-01-29 21:39:27 +0000

    initial commit

---
+++ src/main/java/dev/crypts/simpleVoiceChatMinimumVolume/SimpleVoiceChatMinimumVolume.java
@@ -0,0 +1,10 @@
+package dev.crypts.simpleVoiceChatMinimumVolume;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SimpleVoiceChatMinimumVolume implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
